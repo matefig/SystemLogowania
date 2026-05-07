4effe874c9a9c166fd7651ceb32136b5cf80e759
Mateusz Figiel <mateuszfig.ko@gmail.com>
2026-05-07 18:56:28 +0200

    "poprawki"

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-# Importujemy nasz router autentykacji oraz bazę danych
 from app.routers import auth
 from app.database import engine, Base
 
-# Tworzymy tabele w bazie danych (jeśli jeszcze nie istnieją)
 Base.metadata.create_all(bind=engine)
 
 
@@ -19,20 +16,9 @@ app = FastAPI(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Konfiguracja CORS (Cross-Origin Resource Sharing)
-# Pozwala to na ewentualne podłączenie frontendu (np. w React lub Vue) w przyszłości
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # W produkcji zmienilibyśmy to na konkretną domenę!
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-# Podpinamy nasz router z pliku app/routers/auth.py
 app.include_router(auth.router)
 
-# Prosty endpoint powitalny
 @app.get("/")
 def read_root():
     # Zwraca plik HTML jako główną stronę
